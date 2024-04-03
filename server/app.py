7b228d20cc0df8161a0e9a48c382ba4ea4ecd27b
miriam <miriamwangui525@gmail.com>
2024-04-03 23:16:47 +0300

    feat:python operation with flask routing and views

--- server/app.py
+++ server/app.py
@@ -1,8 +1,41 @@
-#!/usr/bin/env python3
-
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:param>')
+def print_string(param):
+    print(param)
+    return param
+
+@app.route('/count/<int:param>')
+def count(param):
+    numbers = '\n'.join(str(num) for num in range(param))
+    return numbers
+
+@app.route('/math/<float:num1>/<operation>/<float:num2>')
+def math(num1, operation, num2):
+    result = None
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            return "Error: Division by zero"
+    elif operation == 'mod':
+        result = num1 % num2
+    else:
+        return "Invalid operation"
+
+    return str(result)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/testing/app_test.py
+++ server/testing/app_test.py
@@ -1,76 +1,78 @@
 import io
 import sys
+import pytest
+from app import app 
 
-from app import app
+@pytest.fixture
+def client():
+    """Create a test client using the Flask application."""
+    with app.test_client() as client:
+        yield client
 
-class TestApp:
-    '''Flask application in flask_app.py'''
+def test_index(client):
+    """Test the index route."""
+    response = client.get('/')
+    assert b"<h1>Python Operations with Flask Routing and Views</h1>" in response.data
 
-    def test_index_route(self):
-        '''has a resource available at "/".'''
-        response = app.test_client().get('/')
-        assert(response.status_code == 200)
+def test_print_route(client):
+    '''has a resource available at "/print/<parameter>".'''
+    response = client.get('/print/hello')
+    assert(response.status_code == 200)
 
-    def test_index_text(self):
-        '''displays "Python Operations with Flask Routing and Views" in h1 in browser.'''
-        response = app.test_client().get('/')
-        assert(response.data.decode() == '<h1>Python Operations with Flask Routing and Views</h1>')
+def test_print_text(client):
+    '''displays text of route in browser.'''
+    response = client.get('/print/hello')
+    assert(response.data.decode() == 'hello')
 
-    def test_print_route(self):
-        '''has a resource available at "/print/<parameter>".'''
-        response = app.test_client().get('/print/hello')
-        assert(response.status_code == 200)
+def test_print_text_in_console(client):
+    '''displays text of route in console.'''
+    captured_out = io.StringIO()
+    sys.stdout = captured_out
+    client.get('/print/hello')
+    sys.stdout = sys.__stdout__
+    assert(captured_out.getvalue() == 'hello\n')
 
-    def test_print_text(self):
-        '''displays text of route in browser.'''
-        response = app.test_client().get('/print/hello')
-        assert(response.data.decode() == 'hello')
+def test_count_route(client):
+    '''has a resource available at "/count/<parameter>".'''
+    response = client.get('/count/5')
+    assert(response.status_code == 200)
 
-    def test_print_text_in_console(self):
-        '''displays text of route in console.'''
-        captured_out = io.StringIO()
-        sys.stdout = captured_out
-        app.test_client().get('/print/hello')
-        sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == 'hello\n')
+def test_count_range_10(client):
+    '''counts through range of parameter in "/count/<parameter" on separate lines.'''
+    response = client.get('/count/10')
+    count = '0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n'
+    print("Expected count:", count)
+    print("Actual response:", response.data.decode())
+    assert response.data.decode().strip() == count.strip()
 
-    def test_count_route(self):
-        '''has a resource available at "/count/<parameter>".'''
-        response = app.test_client().get('/count/5')
-        assert(response.status_code == 200)
 
-    def test_count_range_10(self):
-        '''counts through range of parameter in "/count/<parameter" on separate lines.'''
-        response = app.test_client().get('/count/10')
-        count = '0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n'
-        assert(response.data.decode() == count)
+def test_math_route(client):
+    '''has a resource available at "/math/<parameters>".'''
+    response = client.get('/math/5/+/5')
+    assert(response.status_code == 200)
 
-    def test_math_route(self):
-        '''has a resource available at "/math/<parameters>".'''
-        response = app.test_client().get('/math/5/+/5')
-        assert(response.status_code == 200)
+def test_math_add(client):
+    '''adds parameters in "/math/" resource when operation is "+".'''
+    response = client.get('/math/5/+/5')
+    assert(response.data.decode() == '10')
 
-    def test_math_add(self):
-        '''adds parameters in "/math/" resource when operation is "+".'''
-        response = app.test_client().get('/math/5/+/5')
-        assert(response.data.decode() == '10')
+def test_math_subtract(client):
+    '''subtracts parameters in "/math/" resource when operation is "-".'''
+    response = client.get('/math/5/-/5')
+    assert(response.data.decode() == '0')
 
-    def test_math_subtract(self):
-        '''subtracts parameters in "/math/" resource when operation is "-".'''
-        response = app.test_client().get('/math/5/-/5')
-        assert(response.data.decode() == '0')
+def test_math_multiply(client):
+    '''multiplies parameters in "/math/" resource when operation is "*".'''
+    response = client.get('/math/5/*/5')
+    assert(response.data.decode() == '25')
 
-    def test_math_multiply(self):
-        '''multiplies parameters in "/math/" resource when operation is "*".'''
-        response = app.test_client().get('/math/5/*/5')
-        assert(response.data.decode() == '25')
+def test_math_divide(client):
+    '''divides parameters in "/math/" resource when operation is "div".'''
+    response = client.get('/math/5/div/5')
+    assert(response.data.decode() == '1.0')
 
-    def test_math_divide(self):
-        '''divides parameters in "/math/" resource when operation is "div".'''
-        response = app.test_client().get('/math/5/div/5')
-        assert(response.data.decode() == '1.0')
-    
-    def test_math_modulo(self):
-        '''finds remainder of parameters in "/math/" resource when operation is "%".'''
-        response = app.test_client().get('/math/5/%/5')
-        assert(response.data.decode() == '0')
+def test_math_modulo(client):
+    '''finds remainder of parameters in "/math/" resource when operation is "%".'''
+    response = client.get('/math/5/mod/5')  
+    assert response.data.decode() == '0'  
+
